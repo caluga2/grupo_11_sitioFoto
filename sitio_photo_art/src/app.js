28303424b2de80a2887804604bc7eb20d5c77f7a
Ariel Adrián Mosquera <110957994+ArielDigital@users.noreply.github.com>
2022-11-22 01:26:44 -0300

    ajuste CSS variado

--- sitio_photo_art/src/app.js
+++ sitio_photo_art/src/app.js
@@ -2,34 +2,35 @@ const express = require("express");
 
 const app = express();
 
-const path = require('path');
+const path = require("path");
 
-const methodOverride =  require('method-override'); 
+const methodOverride = require("method-override");
 
 app.use(express.static("../public"));
 
 app.set("view engine", "ejs");
 
-app.set('views', path.join(__dirname, '/views')); // Define la ubicación de la carpeta de las Vistas
+app.set("views", path.join(__dirname, "/views")); // Define la ubicación de la carpeta de las Vistas
 
 // **** Middlewares  ****
-app.use(express.static(path.join(__dirname, '../public')));  // Necesario para los archivos estáticos en el folder /public
+app.use(express.static(path.join(__dirname, "../public"))); // Necesario para los archivos estáticos en el folder /public
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(methodOverride('_method')); 
+app.use(methodOverride("_method"));
 
 // **** Template Engine - (don't touch) ****
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views')); // Define la ubicación de la carpeta de las Vistas
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views")); // Define la ubicación de la carpeta de las Vistas
 
 // **** Route System require and use() ****
-const mainRouter = require('./routes/main'); // Rutas main
-const productsRouter = require('./routes/products'); // Rutas /products
+const mainRouter = require("./routes/main"); // Rutas main
+const productsRouter = require("./routes/products"); // Rutas /products
 
-app.use('/', mainRouter);
-app.use('/products', productsRouter);
+app.use("/", mainRouter);
+app.use("/products", productsRouter);
 
-app.listen(3001, () => {
+const PUERTO = process.env.PORT || 3001;
+
+app.listen(PUERTO, () => {
   console.log("Servidor funcionando en puerto", 3001);
 });
-
