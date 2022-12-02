3796cd52ea641cf60febc940a68bea4cbdb67443
nicocabrerafit2 <nicocabrera8@outlook.com>
2022-12-01 23:26:47 -0300

    ignoro node_modules

--- sitio_photo_art/src/app.js
+++ sitio_photo_art/src/app.js
@@ -8,10 +8,6 @@ const methodOverride = require("method-override");
 
 app.use(express.static("../public"));
 
-app.set("view engine", "ejs");
-
-app.set("views", path.join(__dirname, "/views")); // Define la ubicación de la carpeta de las Vistas
-
 // **** Middlewares  ****
 app.use(express.static(path.join(__dirname, "../public"))); // Necesario para los archivos estáticos en el folder /public
 app.use(express.urlencoded({ extended: false }));
