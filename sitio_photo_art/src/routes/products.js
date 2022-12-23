8eb743d7692f0cc70c0c5ab2df144ee219181453
nicocabrerafit2 <nicocabrera8@outlook.com>
2022-12-22 22:33:49 -0300

    modifico extension de imagenes

--- sitio_photo_art/src/routes/products.js
+++ sitio_photo_art/src/routes/products.js
@@ -1,50 +1,46 @@
 // **** Require's ****
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const multer = require('multer');
+const multer = require("multer");
+const path = require("path");
 // **** Controller Require ****
-const productsController = require('../controllers/productsController');
+const productsController = require("../controllers/productsController");
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, '../public/images')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix)
-    }
-  })
-  
-  const upload = multer({ storage: storage })
+  destination: function (req, file, cb) {
+    cb(null, "../public/images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
 
-  router.get("/Producto", (req, res) => {
-    res.render("Producto");
-  });
-  
-  router.get("/productAdd", (req, res) => {
-    res.render("productAdd");
-  });
+const upload = multer({ storage: storage });
 
+router.get("/Producto", (req, res) => {
+  res.render("Producto");
+});
 
-/* REVISAR TODO LO ABAJO */ 
+router.get("/productAdd", (req, res) => {
+  res.render("productAdd");
+});
 
-/* GET ALL PRODUCTS */ 
-router.get('/', productsController.index); 
+/* REVISAR TODO LO ABAJO */
 
-/* CREATE ONE PRODUCT */ 
-router.get('/productAdd', productsController.productAdd); 
-router.post('/',upload.single("imagen"), productsController.store); 
+/* GET ALL PRODUCTS */
+router.get("/", productsController.index);
 
+/* CREATE ONE PRODUCT */
+router.get("/productAdd", productsController.productAdd);
+router.post("/", upload.single("imagen"), productsController.store);
 
-/* GET ONE PRODUCT */ 
-router.get('/detail/:id', productsController.detail); 
+/* GET ONE PRODUCT */
+router.get("/detail/:id", productsController.detail);
 
-/* EDIT ONE PRODUCT */ 
-router.get('/edit/:id', productsController.edit); 
-router.patch('/edit/:id', productsController.update); 
+/* EDIT ONE PRODUCT */
+router.get("/edit/:id", productsController.edit);
+router.patch("/edit/:id", productsController.update);
 
-
-/* DELETE ONE PRODUCT***/ 
-router.delete('/delete/:id', productsController.destroy); 
-
+/* DELETE ONE PRODUCT***/
+router.delete("/delete/:id", productsController.destroy);
 
 module.exports = router;
