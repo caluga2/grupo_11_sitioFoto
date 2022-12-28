bf4a3db915519c74607da90f525fa141838cf0bc
nicocabrerafit2 <nicocabrera8@outlook.com>
2022-12-28 19:34:21 -0300

    incorporo express validator para formulario usuarios nuevos

--- sitio_photo_art/src/controllers/userController.js
+++ sitio_photo_art/src/controllers/userController.js
@@ -1,12 +1,13 @@
 const fs = require("fs");
 const path = require("path");
 const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
 const usersFilePath = path.join(__dirname, "../data/users.json");
 const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
 
 const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-const salt = bcrypt.genSaltSync(10);
+
 const controller = {
   index: (req, res) => {
     res.resdirect("home");
@@ -22,23 +23,30 @@ const controller = {
   // Create -  Method to store
 
   store: (req, res) => {
-    let image;
-    if (req.file != undefined) {
-      image = req.file.filename;
-    } else {
-      image = "default-image.png";
-    }
-    let newUser = {
-      id: users[users.length - 1].id + 1,
-      nombre: req.body.nombre,
-      email: req.body.email,
-      contrasena: bcrypt.hashSync(req.body.contrasena, 10),
+    let errores = validationResult(req);
+    if (errores.isEmpty()) {
+      let image;
+      if (req.file != undefined) {
+        image = req.file.filename;
+      } else {
+        image = "default-image.png";
+      }
 
-      image,
-    };
-    users.push(newUser);
-    fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "));
-    res.redirect("/");
+      let newUser = {
+        id: users[users.length - 1].id + 1,
+        nombre: req.body.nombre,
+        email: req.body.email,
+        contrasena: bcrypt.hashSync(req.body.contrasena, 10),
+        image,
+      };
+      users.push(newUser);
+      fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "));
+      res.redirect("/");
+    } else {
+      res.render("register", {
+        errores: errores.array(),
+      });
+    }
   },
 };
 
--- sitio_photo_art/src/routes/users.js
+++ sitio_photo_art/src/routes/users.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
-
+const { body } = require("express-validator");
 // **** Controller Require ****
 const userController = require("../controllers/userController");
 const storage = multer.diskStorage({
@@ -14,9 +14,23 @@ const storage = multer.diskStorage({
     cb(null, Date.now() + path.extname(file.originalname));
   },
 });
-
+const validaciones = [
+  body("nombre").notEmpty().withMessage("Debe introducir su nombre"),
+  body("email")
+    .isEmail()
+    .withMessage("Debe introducir un mail válido (xxx@xx.com)"),
+  body("contrasena")
+    .notEmpty()
+    .isLength({ min: 6 })
+    .withMessage("Debe introducir una contraseña más larga"),
+  body("contrasena1").custom((value, { req }) => {
+    if (value !== req.body.contrasena) {
+      throw new Error("Las contraseñas no coinciden");
+    }
+    return true;
+  }),
+];
 const upload = multer({ storage: storage });
-
 
 //Actualizar para lo nuestro
 router.get("/", userController.index);
@@ -25,6 +39,6 @@ router.get("/", userController.index);
 router.get("/register", userController.register);
 
 router.get("/userList", userController.list);
-router.post("/", upload.single("imagen"), userController.store);
+router.post("/", upload.single("imagen"), validaciones, userController.store);
 
 module.exports = router;
