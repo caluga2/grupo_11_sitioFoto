c9f4ee9fe742340b870b6842be4fbb3166f2681b
nico cabrera <nicocabrera8@outlook.com>
2023-01-11 23:44:17 -0300

    intento agregar logica para mostrar el usuario

--- sitio_photo_art/src/app.js
+++ sitio_photo_art/src/app.js
@@ -18,7 +18,13 @@ app.use(express.static(path.join(__dirname, "../public"))); // Necesario para lo
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(methodOverride("_method"));
-app.use(session({ secret: "Son las session" }));
+app.use(
+  session({
+    secret: "Son las session",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 // **** Template Engine - (don't touch) ****
 app.set("view engine", "ejs");
--- sitio_photo_art/src/controllers/userController.js
+++ sitio_photo_art/src/controllers/userController.js
@@ -1,11 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 const bcrypt = require("bcryptjs");
-const {validationResult} = require("express-validator");
+const { validationResult } = require("express-validator");
 
 const usersFilePath = path.join(__dirname, "../data/users.json");
 const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
-
 
 const controller = {
   index: (req, res) => {
@@ -19,20 +18,20 @@ const controller = {
     return res.render("login");
   },
   procesLogin: (req, res) => {
-
     for (let i = 0; i < users.length; i++) {
       if (users[i].email == req.body.email) {
         if (bcrypt.compareSync(req.body.contrasenaLogin, users[i].contrasena)) {
           var usuarioALogearse = users[i];
         }
       }
-      console.log(req.body.email)
     }
     if (usuarioALogearse == undefined) {
-      return res.render("errorLogin")
-    }else {
-      return res.render("home");}
-    },
+      return res.render("errorLogin");
+    } else {
+      req.session.usuarioLogueado = usuarioALogearse;
+      return res.render("home", { user: req.session.usuarioLogueado });
+    }
+  },
 
   register: (req, res) => {
     res.render("register");
@@ -43,6 +42,17 @@ const controller = {
     let errores = validationResult(req);
     if (errores.isEmpty()) {
       let image;
+      // seguir revisando para que se fije si el mail ya se encuentra registrado
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].email == req.body.email) {
+          return res.render("register", {
+            errores: {
+              msg: "Este email ya está registrado",
+            },
+          });
+        }
+      }
+      // --------------------------------- hasta aca ---------------------
       if (req.file != undefined) {
         image = req.file.filename;
       } else {
