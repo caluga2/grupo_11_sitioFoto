444bf8badaddfbbb83ccd88e548265392399e81e
nicocabrerafit2 <nicocabrera8@outlook.com>
2023-03-03 23:58:30 -0300

    agrego cantidad de productos y usuarios

--- sitioFotoReactDashBoard/src/components/MainBody.js
+++ sitioFotoReactDashBoard/src/components/MainBody.js
@@ -2,12 +2,18 @@ import React from "react";
 import "../../src/App.css";
 import ImgBody from "./ImgBody";
 import TopBar from "./TopBar";
+import TotalProductos from "./TotalProductos";
+import TotalUsuarios from "./TotalUsuarios";
 
 function MainBody() {
   return (
     <React.Fragment>
       <div>
         <TopBar />
+        <div style={{ display: "flex", alignContent: "space-between" }}>
+          <TotalProductos />
+          <TotalUsuarios />
+        </div>
         <ImgBody />
       </div>
     </React.Fragment>
